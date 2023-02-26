d72aa12813e6cffebf21a40ae79af434e468432e
JavaHutt <alxmer111@gmail.com>
2023-02-26 17:03:58 +0400

    sass installed

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,6 +1,8 @@
+import styles from './Menu.module.scss';
+
 const Menu: React.FC = () => {
     return (
-        <div>Menu here</div>
+        <div className={styles.menu}>Menu here</div>
     );
 };
 
