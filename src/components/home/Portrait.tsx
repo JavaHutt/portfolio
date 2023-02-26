0f47871b945e74c52ddede0c461c511f9da66b1f
JavaHutt <alxmer111@gmail.com>
2023-02-26 19:19:44 +0400

    portrait stickers

--- src/components/home/Portrait.tsx
+++ src/components/home/Portrait.tsx
@@ -1,9 +1,15 @@
 import styles from './Portrait.module.scss';
 import photo from '../../assets/photo.jpg';
+import diamond from '../../assets/diamond.gif';
+import duck from '../../assets/duck.png';
+import scorpion from '../../assets/scorpion.png';
 
 const Portrait: React.FC = () => (
     <section className={styles.portrait}>
         <div className={styles.card}>
+            <img className={`${styles.card__sticker} ${styles.card__sticker_first}`} src={duck} alt="duck" />
+            <img className={`${styles.card__sticker} ${styles.card__sticker_second}`} src={diamond} alt="diamond" />
+            <img className={`${styles.card__sticker} ${styles.card__sticker_third}`} src={scorpion} alt="scorpion" />
             <img className={styles.card__image} src={photo} alt="alex" />
             <div className={styles.card__container}>
                 <h4><strong>Alexander Karle</strong></h4>
