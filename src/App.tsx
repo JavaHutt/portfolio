d793a2b2eda049873f06557743ca9b96514ab9ab
JavaHutt <alxmer111@gmail.com>
2023-02-26 19:43:14 +0400

    router init

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react';
 import Page from './components/Page';
 import './App.css';
 
 const App: React.FC = () => {
-    const [count, setCount] = useState(0);
-
     return (
         <div className="App">
             <Page />
--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,16 +1,30 @@
-import Menu from './Menu';
+import styles from './Page.module.scss';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Home from './home/Home';
+import Skills from './skills/Skills';
+import Projects from './projects/projects';
+import Contact from './contact/contact';
 
 const Page: React.FC = () => {
+    const getNavLink = (path: string, label: string) => <Link className={styles.list__link} to={path}>{label}</Link>;
+
     return (
-        <>
-            <header>
-                <Menu />                
-            </header>
-            <main>
-                <Home />
-            </main>
-        </>
+        <Router>
+            <nav className={styles.nav}>
+                <ul className={styles.list}>
+                    {getNavLink('/', 'Home')}
+                    {getNavLink('/skills', 'My skills')}
+                    {getNavLink('/projects', 'Projects')}
+                    {getNavLink('/contact', 'Contact')}
+                </ul>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/skills" element={<Skills />} />
+                <Route path="/projects" element={<Projects />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
+        </Router>
     );
 };
 
