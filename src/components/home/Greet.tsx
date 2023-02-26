b4a986e44ee39676edb31d7db2868916d2379986
JavaHutt <alxmer111@gmail.com>
2023-02-26 19:02:04 +0400

    photo card

--- src/components/home/Greet.tsx
+++ src/components/home/Greet.tsx
@@ -1,7 +1,7 @@
 import styles from './Greet.module.scss';
 
 const Greet: React.FC = () => (
-    <div className={styles.greet}>
+    <section className={styles.greet}>
         <div className={styles.text}>
             <h1 className={styles.header}>Hello!👋</h1>
             <p className={styles.bio}>My name is Alex</p>
@@ -11,10 +11,10 @@ const Greet: React.FC = () => (
             <div className={styles.badge}>
                 <p>Backend Developer</p>
                 <p>&</p>
-                <p>Frontent Developer</p>
+                <p>Frontend Developer</p>
             </div>
         </div>
-    </div>
+    </section>
 );
 
 export default Greet;
--- src/components/home/Portrait.tsx
+++ src/components/home/Portrait.tsx
@@ -1,9 +1,15 @@
 import styles from './Portrait.module.scss';
+import photo from '../../assets/photo.jpg';
 
-const Portrait: React.FC = () => {
-    return (
-        <section className={styles.portrait}></section>
-    );
-};
+const Portrait: React.FC = () => (
+    <section className={styles.portrait}>
+        <div className={styles.card}>
+            <img className={styles.card__image} src={photo} alt="alex" />
+            <div className={styles.card__container}>
+                <h4><strong>Alexander Karle</strong></h4>
+            </div>
+        </div>
+    </section>
+);
 
 export default Portrait;
